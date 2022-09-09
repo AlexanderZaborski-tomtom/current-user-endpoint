c72312e469e208714a230f89841687d41bd5bbc2
AlexanderZaborski-tomtom <alexander.zaborski@tomtom.com>
2022-09-09 13:03:28 +0200

    Fixed formatting in Controller

--- custom_endpoint/src/Controller/CustomEndpointController.php
+++ custom_endpoint/src/Controller/CustomEndpointController.php
@@ -11,73 +11,69 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Custom Endpoint Controller class
  */
-
 class CustomEndpointController extends ControllerBase {
 
-    /**
-    * @param AccountInterface $currentUserClass constructor
-    */
-    public function __construct(AccountInterface $currentUser) {
-      $this->currentUser = $currentUser;
+  /**
+   * @param AccountInterface $currentUserClass constructor
+   */
+  public function __construct(AccountInterface $currentUser) {
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * Gets the current user
+   */
+  public static function create(ContainerInterface $container): CustomEndpointController {
+    return new static(
+      $container->get('current_user'),
+    );
+  }
+
+  /**
+   * @return JsonResponse
+   */
+  public function renderApi(): JsonResponse {
+    return new JsonResponse([
+      'data' => $this->getResults(),
+      'method' => 'GET',
+    ]);
+  }
+
+  /**
+   * Get current user data
+   * or return an error message
+   */
+  public function getResults(): array {
+
+    //Try to get user data
+    try {
+      $username = $this->currentUser->getAccountName();
+      $created = $this->currentUser->get['created'];
+      $moddate = $this->currentUser->getLastAccessedTime();
+      $lastlogon = $this->currentUser->getLastAccessedTime();
+
+      if ($username == "") {
+        return "You are not logged in.";
+      }
+      else {
+        //user data output
+        return [
+          [
+            "username" => $username,
+            "created" => date("d F Y H:i:s", $created),
+            "moddate" => date("d F Y H:i:s", $moddate),
+            "lastlogon" => date("d F Y H:i:s", $lastlogon),
+          ],
+        ];
+      }
     }
-
-    /**
-    * Gets the current user
-    */
-    public static function create(ContainerInterface $container) : CustomEndpointController {
-      return new static(
-        $container->get('current_user'),
-      );
+    //Return error message
+    catch (\Throwable $th) {
+      //throw $th;
+      return [
+        "error" => $th->getMessage(),
+      ];
     }
-
-    /**
-    * @return JsonResponse
-    */
-    public function renderApi() : JsonResponse {
-      return new JsonResponse([
-        'data' => $this->getResults(),
-        'method' => 'GET',
-      ]);
-    }
-
-    /**
-    * Get current user data
-    * or return an error message
-    */
-    public function getResults() : array {
-
-      //Try to get user data
-      try {
-         $username = $this->currentUser->getAccountName();
-         //$created = $this->currentUser->get('created')->getValue();
-         $created = $this->currentUser->get['created'];
-         $moddate = $this->currentUser->getLastAccessedTime();
-         $lastlogon = $this->currentUser->getLastAccessedTime();
-
-         if ($username == "") {
-             return "You are not logged in.";
-         }
-         else {
-             //user data output
-             return [
-                 [
-                   "username" => $username,
-                   "created" => date("d F Y H:i:s", $created),
-                   "moddate" => date("d F Y H:i:s", $moddate),
-                   "lastlogon" => date("d F Y H:i:s", $lastlogon),
-                 ],
-               ];
-         }
-     }
-     //Return error message
-     catch (\Throwable $th) {
-         //throw $th;
-     return [
-       "error" => $th->getMessage(),
-     ];
-     }
-   }
-
+  }
 
 }
-
