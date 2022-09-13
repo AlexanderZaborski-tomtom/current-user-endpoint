567dedbbd559816b213e72843e8c0080b2340684
AlexanderZaborski-tomtom <alexander.zaborski@tomtom.com>
2022-09-13 13:11:25 +0200

    New file for timestamp user service

--- custom_endpoint/src/Controller/TimestampUsersController.php
+++ custom_endpoint/src/Controller/TimestampUsersController.php
@@ -4,16 +4,25 @@ namespace Drupal\custom_endpoint\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Timestamp Users class
  */
 class TimestampUsersController extends ControllerBase {
 
-  function renderApi(): JsonResponse {
-    $start_date = \Drupal::routeMatch()->getRawParameter('start_date');
-    $end_date = \Drupal::routeMatch()->getRawParameter('end_date');
+  private $start_date;
+  private $end_date;
 
+  /**
+   * Get parameter values
+   */
+  function getParams(Request $request) {
+    $start_date = $request->get('start_date');
+    $end_date = $request->get('end_date');
+  }
+
+  function renderApi(): JsonResponse {
     return new JsonResponse([
       'Start date' => $start_date,
       'End date' => $end_date
